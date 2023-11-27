7f626ee5e0f4530ab8d186679ee0a985aada559f
matheus676 <matheus.m.monteiro2003@gmail.com>
2023-11-27 20:55:38 -0300

    MODELS 1

--- projetosuv/apps/lotedevacinas/models.py
+++ projetosuv/apps/lotedevacinas/models.py
@@ -1,3 +1,16 @@
+from dataclasses import dataclass
+from msilib.schema import Class
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    fabricante = models.CharField('Fabricante', max_length=50)
+    data_de_fabricacao = models.DateField('Data de Fabricação', max_length=100)
+    
+    class Meta:
+        verbose_name = 'Lote de Vacina'
+        verbose_name_plural = 'Lote de Vacinas'
+        ordering =['id']
+
+    def __str__(self):
+        return self
