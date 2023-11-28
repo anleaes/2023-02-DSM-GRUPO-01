2fe2083d1da0f772610784bc48ab47eb927dfc9d
matheus676 <matheus.m.monteiro2003@gmail.com>
2023-11-28 19:20:07 -0300

    VIEWS 1 - IMPORTS

--- projetosuv/apps/lotedevacinas/views.py
+++ projetosuv/apps/lotedevacinas/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
-
+from .models import LoteDeVacina
+from restframework import viewsets
+from .serializer import LoteDeVacinaSerializer
 # Create your views here.
