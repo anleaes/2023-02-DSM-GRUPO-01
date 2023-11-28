4c82834369f7ecabf9bb6672b3c4e77402b632cf
matheus676 <matheus.m.monteiro2003@gmail.com>
2023-11-28 19:20:54 -0300

    VIEWS 2 - CLASSE

--- projetosuv/apps/lotedevacinas/views.py
+++ projetosuv/apps/lotedevacinas/views.py
@@ -3,3 +3,6 @@ from .models import LoteDeVacina
 from restframework import viewsets
 from .serializer import LoteDeVacinaSerializer
 # Create your views here.
+class LoteDeVacinasViewSet(viewsets.ModelViewSet):
+    queryset = LoteDeVacina.objects.all()
+    serializer_class = LoteDeVacinaSerializer  
