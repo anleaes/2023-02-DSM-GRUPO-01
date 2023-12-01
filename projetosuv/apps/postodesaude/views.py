f71937cfcebbe3f0e962273522d0732330b7ea5a
biazettogui <guilherme02.biazetto@gmail.com>
2023-12-01 20:19:06 -0300

    views1

--- projetosuv/apps/postodesaude/views.py
+++ projetosuv/apps/postodesaude/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .models import PostoDeSaude
+from rest_framework import viewsets
+from .serializer import PostoDeSaudeSerializer
 
 # Create your views here.
