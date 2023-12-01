66d14296528ab406f82aaef577f87b612e4e9052
biazettogui <guilherme02.biazetto@gmail.com>
2023-12-01 20:48:38 -0300

    models2

--- projetosuv/apps/pacientes/models.py
+++ projetosuv/apps/pacientes/models.py
@@ -2,6 +2,16 @@ from django.db import models
 
 # Create your models here.
 
-class Pacientes(models.Model):
+class Paciente(models.Model):
     nome = models.CharField('Nome', max_length=50)
-    data_de_nascimento = models.DateField('Data de Nascimento', max_length=100)
+    data_de_nascimento = models.DateField('Data de Nascimento')
+    endereco = models.TextField('Endereço', max_length=200)
+    telefone = models.CharField('Telefone')
+
+    class Meta:
+        verbose_name = 'Paciente'
+        verbose_name_plural = 'Pacientes'
+        ordering =['id']
+
+    def __str__(self):
+        return self
