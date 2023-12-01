a814a091c279456bc5d556facc6726200c191f61
biazettogui <guilherme02.biazetto@gmail.com>
2023-12-01 20:06:19 -0300

    models1

--- projetosuv/apps/postodesaude/models.py
+++ projetosuv/apps/postodesaude/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class PostoDeSaude(models.Model):
+    nome = models.CharField('Nome', max_length=50)
+    endereco = models.TextField('Endereço', max_length=100)
+    horario_funcionamento = models.TimeField('Horário de Funcionamento')
