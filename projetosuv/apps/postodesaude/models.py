34bdb8b320eca6fe9f5eaf09cd62638eb575d8fd
biazettogui <guilherme02.biazetto@gmail.com>
2023-12-01 20:10:01 -0300

    models2

--- projetosuv/apps/postodesaude/models.py
+++ projetosuv/apps/postodesaude/models.py
@@ -6,3 +6,11 @@ class PostoDeSaude(models.Model):
     nome = models.CharField('Nome', max_length=50)
     endereco = models.TextField('Endereço', max_length=100)
     horario_funcionamento = models.TimeField('Horário de Funcionamento')
+
+    class Meta:
+        verbose_name = 'Posto de Saúde'
+        verbose_name_plural = 'Postos de Saúde'
+        ordering =['id']
+
+    def __str__(self):
+        return self
