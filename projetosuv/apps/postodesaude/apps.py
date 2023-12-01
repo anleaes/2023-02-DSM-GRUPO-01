7d4a66d0a36233a1df52c8c1a157df8b59d09e0a
biazettogui <guilherme02.biazetto@gmail.com>
2023-12-01 19:56:34 -0300

    apps1

---
+++ projetosuv/apps/postodesaude/apps.py
@@ -0,0 +1,8 @@
+from django.apps import AppConfig
+
+
+class PostodesaudeConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'postodesaude'
+    verbose_name = 'Posto de Saúde'
+    
