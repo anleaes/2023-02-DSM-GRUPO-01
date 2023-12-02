2b560ab57fa0aec2590e922a2c15bc57051998cc
biazettogui <guilherme02.biazetto@gmail.com>
2023-12-01 22:32:13 -0300

    models3

--- projetosuv/apps/agendamentos/models.py
+++ projetosuv/apps/agendamentos/models.py
@@ -3,7 +3,7 @@ from pacientes.models import Paciente
 from postodesaude.models import PostoDeSaude
 # Create your models here.
 
-class Agendamentos(models.Model):
+class Agendamento(models.Model):
     paciente = models.ForeignKey(Paciente, verbose_name = 'Paciente', on_delete = models.CASCADE )
     postodesaude = models.ForeignKey(PostoDeSaude, verbose_name ='Posto de Saúde', on_delete = models.CASCADE)
     datahora = models.DateTimeField('Data e Hora')
