fa8ca65c6a9cdf5f21f5a9c5fae19890372aee37
biazettogui <guilherme02.biazetto@gmail.com>
2023-12-01 22:14:13 -0300

    views2

--- projetosuv/apps/agendamentos/views.py
+++ projetosuv/apps/agendamentos/views.py
@@ -4,3 +4,7 @@ from rest_framework import viewsets
 from .serializer import AgendamentoSerializer
 
 # Create your views here.
+
+class AgendamentoViewSet(viewsets.ModelViewSet):
+    queryset = Agendamento.objects.all()
+    serializer_class = AgendamentoSerializer
