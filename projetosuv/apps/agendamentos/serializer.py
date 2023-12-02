aaa3b661263128c0792b479e8bb14a45e543c8ec
biazettogui <guilherme02.biazetto@gmail.com>
2023-12-01 22:07:47 -0300

    serializer2

--- projetosuv/apps/agendamentos/serializer.py
+++ projetosuv/apps/agendamentos/serializer.py
@@ -1,2 +1,7 @@
 from .models import Paciente
 from rest_framework import serializers
+
+class AgendamentoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Agendamento
+        fields = '__all__'
