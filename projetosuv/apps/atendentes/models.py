c9076e9e085124a72a08e56337ff818aa9605083
sergiodeoliveira02 <sergiodeoliveira02@gmail.com>
2023-12-03 22:32:32 -0300

    Models1

--- projetosuv/apps/atendentes/models.py
+++ projetosuv/apps/atendentes/models.py
@@ -1,3 +1,7 @@
 from django.db import models
-
+from postodesaude.models import PostoDeSaude
 # Create your models here.
+class Especialidade(models.Model):
+    nome = models.CharField('Nome', max_length=50)
+    especialidade = models.CharField('Especialidade', max_length=50)
+    
