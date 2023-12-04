ec27eb008a561cbb676a1c9aa8695fef003d35e0
sergiodeoliveira02 <sergiodeoliveira02@gmail.com>
2023-12-03 22:43:32 -0300

    Views2

--- projetosuv/apps/atendentes/views.py
+++ projetosuv/apps/atendentes/views.py
@@ -1,3 +1,7 @@
 from .models import Atendente
 from rest_framework import viewsets
 from .serializer import AtendenteSerializer
+
+class AtendenteViewSet(viewsets.ModelViewSet):
+    queryset = Atendente.objects.all()
+    serializer_class = AtendenteSerializer
