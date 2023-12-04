77f867901ae05912987972f2d4614d64489c1e8a
sergiodeoliveira02 <sergiodeoliveira02@gmail.com>
2023-12-03 22:36:01 -0300

    Models2

--- projetosuv/apps/atendentes/models.py
+++ projetosuv/apps/atendentes/models.py
@@ -4,4 +4,12 @@ from postodesaude.models import PostoDeSaude
 class Especialidade(models.Model):
     nome = models.CharField('Nome', max_length=50)
     especialidade = models.CharField('Especialidade', max_length=50)
+    posto_de_saude = models.ForeignKey(PostoDeSaude, verbose_name= 'Posto de Saúde', on_delete= models.CASCADE)
     
+    class Meta:
+        verbose_name = 'Atendente'
+        verbose_name_plural = 'Atendentes'
+        ordering =['id']
+
+    def __str__(self):
+        return self
