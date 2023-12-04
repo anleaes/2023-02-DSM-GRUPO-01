859aeda87fc5e05993f5e0313a4af971d31eda15
sergiodeoliveira02 <sergiodeoliveira02@gmail.com>
2023-12-03 23:40:02 -0300

    Views1

--- projetosuv/apps/historicosdevacinacao/views.py
+++ projetosuv/apps/historicosdevacinacao/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .models import HistoricoDeVacinacao
+from rest_framework import viewsets
+from .serializer import HistoricoDeVacinacaoSerializer
 
 # Create your views here.
