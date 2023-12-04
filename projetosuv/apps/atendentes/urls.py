6101745041fcc3992df0f05076f4a540d4ea23bf
sergiodeoliveira02 <sergiodeoliveira02@gmail.com>
2023-12-03 22:45:47 -0300

    Urls3

--- projetosuv/apps/atendentes/urls.py
+++ projetosuv/apps/atendentes/urls.py
@@ -5,7 +5,7 @@ from rest_framework import routers
 app_name = 'atendentes'
 
 router = routers.DefaultRouter()
-router.register('', views.CategoryViewSet, basename='atendentes')
+router.register('', views.AtendenteViewSet, basename='atendentes')
 
 urlpatterns = [
     path('', include(router.urls) )
