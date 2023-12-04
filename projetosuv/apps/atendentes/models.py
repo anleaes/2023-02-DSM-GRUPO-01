44e02937df89c2643efd34d30e029f42b9f8ac55
sergiodeoliveira02 <sergiodeoliveira02@gmail.com>
2023-12-03 22:38:53 -0300

    Models3

--- projetosuv/apps/atendentes/models.py
+++ projetosuv/apps/atendentes/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from postodesaude.models import PostoDeSaude
 # Create your models here.
-class Especialidade(models.Model):
+class Atendente(models.Model):
     nome = models.CharField('Nome', max_length=50)
     especialidade = models.CharField('Especialidade', max_length=50)
     posto_de_saude = models.ForeignKey(PostoDeSaude, verbose_name= 'Posto de Saúde', on_delete= models.CASCADE)
